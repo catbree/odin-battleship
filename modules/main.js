bfae9cb8b79d3cd781f749dda6b06ac6d356bb4c
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-05-23 16:12:01 +0800

    installed http-server to run on local without using webpack to bundle

--- modules/main.js
+++ modules/main.js
@@ -1,3 +1,23 @@
 import { Gameboard } from "./gameboard.js"
 import { Ship } from "./ship.js"
 import { Player } from "./player.js"
+
+console.log(`hello world`);
+
+// function startGame() {
+//     const player1 = new Player('Player','player');
+//     const player2 = new Player('Player','computer');
+
+//     //place ships for both players
+//     player1.gameboard.placeShip(new Ship(2), [0, 0]);
+//     player1.gameboard.placeShip(new Ship(2), [2, 2]);
+//     player1.gameboard.placeShip(new Ship(3), [3, 6]);
+//     player1.gameboard.placeShip(new Ship(4), [5, 7]);
+//     player1.gameboard.placeShip(new Ship(5), [4, 9]);
+
+//     player2.gameboard.placeShip(new Ship(2), [5, 0]);
+//     player2.gameboard.placeShip(new Ship(2), [8, 3]);
+//     player2.gameboard.placeShip(new Ship(3), [6, 8]);
+//     player2.gameboard.placeShip(new Ship(4), [2, 5]);
+//     player2.gameboard.placeShip(new Ship(5), [0, 4]);
+// }
