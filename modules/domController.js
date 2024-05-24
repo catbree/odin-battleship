912ded0e7d2884dab4eb6d69cc135a2a2e2cb648
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-05-24 21:05:46 +0800

    created functions to handle computer and player turns to facilitate gameply

--- modules/domController.js
+++ modules/domController.js
@@ -1,24 +1,28 @@
 import { Gameboard } from "./gameboard.js";
 import { Player } from "./player.js";
-import { player1, player2 } from "./main.js"
+import { player1, player2, p1Gameboard, p2Gameboard } from "./main.js"
 
 export function init() {
-    const p1Gameboard = document.querySelector(".gameboard-container.player1")
-    const p2Gameboard = document.querySelector(".gameboard-container.player2")
-    createGameboard(p1Gameboard);
-    createGameboard(p2Gameboard);
+    createGameboard(player1, p1Gameboard);
+    createGameboard(player2, p2Gameboard);
     updateGameboard(player1, p1Gameboard);
     updateGameboard(player2, p2Gameboard);
 
 }
 
-function createGameboard(gameboard) {
+function createGameboard(player, gameboard) {
 
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             const cell = document.createElement('div');
-            cell.className = 'cell';
-            
+            cell.className = 'cell noHit';
+
+            //add class depending on player type
+            if (player.type=='human') {
+                cell.classList.add('human-cell');
+            } else {
+                cell.classList.add('computer-cell');
+            }
             //add custom attribute for subsequent linking to its js equivalent
             cell.dataset.x = j; 
             cell.dataset.y = i;
@@ -36,7 +40,6 @@ export function updateGameboard(player, gameboardElem) {
         const y = parseInt(cellElem.dataset.y);
         const cell = player.gameboard.cells[x][y];
 
-
         if(cell.shipPlaced == true) {
             cellElem.classList.add('ship');
         } else {
@@ -45,10 +48,24 @@ export function updateGameboard(player, gameboardElem) {
 
         if(cell.isHit == true) {
             cellElem.classList.add('hit');
+            cellElem.classList.remove('noHit');
         } else {
             cellElem.classList.remove('hit');
         }
     })
 }
 
+export function addGameboardOutline(gameboard) {
+    gameboard.classList.add("outline");
+}
+
+export function removeGameboardOutline(gameboard) {
+    gameboard.classList.remove("outline");
+}
+
+export function updateInstructionText(string) {
+    const instructionElem = document.querySelector('.instruction-container');
+    instructionElem.textContent = string;
+    
+}
 
--- modules/main.js
+++ modules/main.js
@@ -1,12 +1,14 @@
 import { Gameboard } from "./gameboard.js"
 import { Ship } from "./ship.js"
 import { Player } from "./player.js"
-import { init, updateGameboard } from "./domController.js"
+import { init, updateGameboard, addGameboardOutline, removeGameboardOutline, updateInstructionText } from "./domController.js"
 
 
-//initialise players
+//initialise players & gameboards
 export const player1 = new Player('Sebastian', 'human');
 export const player2 = new Player('Bot', 'computer');
+export const p1Gameboard = document.querySelector(".gameboard-container.player1")
+export const p2Gameboard = document.querySelector(".gameboard-container.player2")
 
 
 //place ships for both players
@@ -22,8 +24,110 @@ player2.gameboard.placeShip(new Ship(3), [6, 4]);
 player2.gameboard.placeShip(new Ship(4), [2, 6]);
 player2.gameboard.placeShip(new Ship(5), [4, 4]);
 
-document.addEventListener('DOMContentLoaded', init);
+//initialise player1 as starting player
+let currentPlayer = player1;
+const computerHits = new Set(); //Set to keep track of computer's hit so computer doesn't hit the same cell more than once.
 
-player1.gameboard.receiveAttack([4, 4]);
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+    startGame();
+});
 
+function startGame() {
+    p2Gameboard.addEventListener('click', handlePlayerTurn);
+    updateInstructionText(`Your turn. Click on a cell on your opponent's gameboard to launch an attack.`);
+}
+
+function handleComputerTurn() {
+
+    let x;
+    let y;
+
+    do {
+        x = generateRandomNumberUpTill9();
+        y = generateRandomNumberUpTill9();
+    } while (computerHits.has(`${x}${y}`));
+
+    computerHits.add(`${x}${y}`); //add a hit coord to set
+
+    player1.gameboard.receiveAttack([x, y]);
+    updateGameboard(player1, p1Gameboard);
+
+    if (!checkEndGameCondition()) {
+        setTimeout(() => {
+            togglePlayer();
+        }, 500);
+    }
+}
+
+function handlePlayerTurn(event) {
+
+    //if not a cell, do nothing
+    if (!event.target.classList.contains('cell')) return;
+    //if clicking on cell that is already hit, do nothing
+    if (!event.target.classList.contains('noHit')) return;
+
+    const x = parseInt(event.target.dataset.x);
+    const y = parseInt(event.target.dataset.y);
+          
+
+    //handle player's move
+    if (currentPlayer === player1) {
+        player2.gameboard.receiveAttack([x, y]);
+        updateGameboard(player2, p2Gameboard);
+    } else {
+        player1.gameboard.receiveAttack([x, y]);
+        updateGameboard(player1, p1Gameboard);
+    }
+
+    if (!checkEndGameCondition()) {
+        togglePlayer();
+    }
+
+}
+
+function togglePlayer() {
+    if (currentPlayer === player1) {
+        currentPlayer = player2;
+        p2Gameboard.removeEventListener('click', handlePlayerTurn);
+        updateInstructionText(`Your opponent's turn to attack.`);
+        removeGameboardOutline(p2Gameboard);
+        addGameboardOutline(p1Gameboard);
+        handleComputerTurn(500);
+    } else {
+        currentPlayer = player1;
+        removeGameboardOutline(p1Gameboard);
+        addGameboardOutline(p2Gameboard);
+        p2Gameboard.addEventListener('click', handlePlayerTurn);
+        updateInstructionText(`Your turn. Click on a cell on your opponent's gameboard to launch an attack.`);
+    }
+}
+
+function checkEndGameCondition() {
+
+    if (player1.gameboard.checkIfAllShipsSunk()) {
+        updateInstructionText(`Computer won, better luck next time!`);
+        endGame();
+        return true;
+    }
+    
+    if (player2.gameboard.checkIfAllShipsSunk()) {
+        updateInstructionText(`You won!`);
+        endGame();
+        return true;
+    }
+
+    return false;
+}
+
+function generateRandomNumberUpTill9() {
+    return Math.floor(Math.random() * 10);
+}
+
+function endGame() {
+    p1Gameboard.removeEventListener('click', handlePlayerTurn);
+    p2Gameboard.removeEventListener('click', handlePlayerTurn);
+    removeGameboardOutline(p1Gameboard);
+    removeGameboardOutline(p2Gameboard);
+}
 
